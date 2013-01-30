996a9595a830391db51543ea80d82b3950f82168
Michael T. O'Sullivan <michaeltosullivan@sbcglobal.net>
2013-01-29 19:50:41 -0800

    First impound

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5de352bf1ebcf3884d62114f9302fbbf49a1a3fbea88211c0a8f22f8cc3e87b859737b3801a5d4d00e6ae17c30c94c412e8f97cae379ae6ef7710ad7d8082938'
